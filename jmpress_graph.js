ea05df06c589048f95df130d438414f61c13e9fb
Simon Funke <funsi@gmx.de>
2012-02-17 21:54:00 +0000

    Some minor tweaks

--- jmpress_graph.js
+++ jmpress_graph.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
    var g = new Graph();
     
+   var nodeDepth = {};
+   var maxDepth = 4.0;
+   nodeDepth["Music"] = 0; 
+   nodeDepth["Editing_course"] = 1; 
+   nodeDepth["Studio_Apprentice"] = 1; 
+   nodeDepth["Music_NBQ_L3"] = 1; 
+   nodeDepth["HND"] = 2; 
+   nodeDepth["Degree"] = 2; 
+   nodeDepth["Label_AIR"] = 2; 
+   nodeDepth["Agency"] = 2; 
+   nodeDepth["Internship"] = 2; 
+   nodeDepth["Agency"] = 3; 
+
    g.addEdge("Music", "Editing_course");
    g.addEdge("Music", "Studio_Apprentice");
    g.addEdge("Music", "Music_NBQ_L3");
@@ -32,7 +45,7 @@ $(document).ready(function() {
          links += '<a href="index.html#/' + t.id + '">Next option: ' + t.id + '</a><br>';
        }
      }
-     $("#impress").append("<div id=" + i + " class='step slide' data-x='" + x*2000 + "' data-y='" + y*2000 + "' data-scale='1'>" + i + "<br>" + links + "</div>");
+     $("#impress").append("<div id=" + i + " class='step slide' data-x='" + x*((maxDepth-nodeDepth[i])/maxDepth)*800 + "' data-y='" + y*((maxDepth-nodeDepth[i])/maxDepth)*800 + "' data-scale='" + (maxDepth-parseFloat(nodeDepth[i])/2)/maxDepth + "'>" + i + "<br>" + links + "</div>");
    }
 
 });
