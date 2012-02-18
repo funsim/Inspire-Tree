29580cd4ed0ed348d663f93c9271dfcaf1caa98d
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-02-18 11:30:10 +0000

    dynamically create graph using json input data

--- jmpress_graph.js
+++ jmpress_graph.js
@@ -1,57 +1,46 @@
+function readJSON(graph, node, parent, depth) {
+  if (!depth) {
+    var depth = 0;
+  }
+  graph.addNode(node.id, { label : node.name, parent : parent, depth : depth, data : node.data });
+  if (parent) {
+    graph.addEdge(parent.id, node.id);
+  }
+  for (child in node.children) {
+    readJSON(graph, node.children[child], node, depth + 1);
+  }
+}
+
 $(document).ready(function() {
    var g = new Graph();
-    
-   var nodeDepth = {};
-   var maxDepth = 4.0;
-   nodeDepth["Music"] = 0; 
-   nodeDepth["Editing_course"] = 1; 
-   nodeDepth["Studio_Apprentice"] = 1; 
-   nodeDepth["Music_NBQ_L3"] = 1; 
-   nodeDepth["HND"] = 2; 
-   nodeDepth["Degree"] = 2; 
-   nodeDepth["Label_AIR"] = 2; 
-   nodeDepth["Agency"] = 2; 
-   nodeDepth["Internship"] = 2; 
-   nodeDepth["Agency"] = 3; 
-
-   g.addEdge("Music", "Editing_course");
-   g.addEdge("Music", "Studio_Apprentice");
-   g.addEdge("Music", "Music_NBQ_L3");
-    
-   g.addEdge("Music_NBQ_L3", "HND");
-   g.addEdge("Music_NBQ_L3", "Degree");
-    
-   g.addEdge("Degree", "Label_AIR");
-   g.addEdge("Degree", "Agency");
-   g.addEdge("Degree", "Internship");
-
-   g.addEdge("Internship", "Agency");
+   readJSON(g, json);
     
    var layouter = new Graph.Layout.Spring(g);
    layouter.layout();
    var i;
    for (i in g.nodes) {
-     var x=g.nodes[i].layoutPosX;
-     var y=g.nodes[i].layoutPosY;
-     var edges = g.nodes[i].edges;
+     node = g.nodes[i]
+     var x=node.layoutPosX;
+     var y=node.layoutPosY;
+     var edges = node.edges;
      var links = '' 
      for (e in edges) {
        var s = edges[e].source;
        var t = edges[e].target;
        if (t.id == i) {
-         links += '<a href="index.html#/' + s.id + '">Previous option: ' + s.id + '</a><br>';
+         links += '<a href="index.html#/' + s.id + '">Previous option: ' + s.label + '</a><br>';
        }
        if (s.id == i) {
-         links += '<a href="index.html#/' + t.id + '">Next option: ' + t.id + '</a><br>';
+         links += '<a href="index.html#/' + t.id + '">Next option: ' + t.label + '</a><br>';
        }
      }
      // A link the directs to the detailed description of this node
-     links += '<a href="index.html#/' + i + '_desc">More information about ' + i + '</a><br>';
+     links += '<a href="index.html#/' + node.id + '_desc">More information about ' + node.label + '</a><br>';
      // A link for the description slide that links back to the main node 
-     desc_back_link = '<a href="index.html#/' + i + '">Get me back to ' + i + '</a><br>';
-     $("#impress").append("<div id='" + i + "' class='step slideleaf' data-x='" + x*800 + "' data-y='" + y*800 + "' data-scale='1'>" + i + "<br>" + links + "</div>");
+     desc_back_link = '<a href="index.html#/' + node.id + '">Get me back to ' + node.label + '</a><br>';
+     $("#impress").append("<div id=" + node.id + " class='step slide' data-x='" + x*800 + "' data-y='" + y*800 + "' data-scale='1'>" + node.label + "<br>" + links + "</div>");
      // Also add a description node for detailed information
-     $("#impress").append("<div id='" + i + "_desc' class='step slide' data-x='" + (x*800+900.0/2+50) + "' data-y='" + (y*800+700.0/2-40) + "' data-scale='0.1'>Videos of " + i + "<br>" + desc_back_link+ "</div>");
+     $("#impress").append("<div id='" + node.id + "_desc' class='step slide' data-x='" + (x*800+900.0/2+50) + "' data-y='" + (y*800+700.0/2-40) + "' data-scale='0.1'>Videos of " + node.label + "<br>" + desc_back_link + "</div>");
    }
 
 });
---
+++ json-data.js
@@ -0,0 +1,191 @@
+var json = {
+      id: "start",
+      name: "Start",
+      data: {},
+      children: [{
+        id: "root1",
+        name: "Degree",
+        data: {},
+        children: []
+      }, {
+        id: "root2",
+        name: "Experience",
+        data: {},
+        children: []
+      }, {
+        id: "root3",
+        name: "Contacts",
+        data: {},
+        children: []
+      }, {
+          id: "music",
+          name: "MUSIC",
+          data: {},
+          children: [{
+            id: "m1",
+            name: "Music Bus NVQ L3",
+            data: { colour: "a" },
+            children: [{
+              id: "m1.1",
+              name: "HND",
+              data: { colour: "a" },
+              children: []
+            }, {
+              id: "m1.2",
+              name: "Degree",
+              data: { colour: "a" },
+              children: [{
+                id: "m1.2.1",
+                name: "",
+                data: {},
+                children: []
+              }, {
+                id: "m1.2.2",
+                name: "",
+                data: {},
+                children: []
+              }, {
+                id: "m1.2.3",
+                name: "",
+                data: {},
+                children: []
+              }, {
+                id: "m1.2.4",
+                name: "Label A&R",
+                data: { colour: "c" },
+                children: []
+              }, {
+                id: "m1.2.5",
+                name: "Agency",
+                data: { colour: "c" },
+                children: []
+              }, {
+                id: "m1.2.6",
+                name: "JNR Product MNG",
+                data: { colour: "c" },
+                children: []
+              }, {
+                id: "m1.2.7",
+                name: "Internship",
+                data: { colour: "b" },
+                children: []
+              }]
+            }]
+          }, {
+            id: "m2",
+            name: "Studio Apprentice",
+            data: { colour: "b" },
+            children: [/*{
+              id: "m1.2.6",
+              name: "JNR Product MNG",
+              data: { colour: "c" },
+              children: []
+            }*/]
+          }, {
+            id: "m3",
+            name: "HNV Assistant",
+            data: { colour: "c" },
+            children: [{
+              id: "m3.1",
+              name: "Manager Retail",
+              data: { colour: "c" },
+              children: []
+            }, {
+              id: "m3.2",
+              name: "Promotion (Events)",
+              data: { colour: "c" },
+              children: []
+            }]
+          }, {
+            id: "m4",
+            name: "Editing Course BTEC",
+            data: { colour: "a" },
+            children: [{
+              id: "m4.1",
+              name: "Level 2 Course",
+              data: { coulour: "a" },
+              children: []
+            }, {
+              id: "m4.2",
+              name: "Editor JNR",
+              data: { coulour: "c" },
+              children: []
+            }]
+          }, {
+            id: "m5",
+            name: "Action on Hearing Loss",
+            data: { colour: "b" },
+            children: []
+          }]
+      }, {
+          id: "basketball",
+          name: "BASKETBALL",
+          data: {},
+          children: [{
+            id: "b1",
+            name: "Teacher Training (PE)",
+            data: { colour: "a" },
+            children: []
+          }, {
+            id: "b2",
+            name: "Volunteering",
+            data: { colour: "b" },
+            children: []
+          }, {
+            id: "b3",
+            name: "Community Coach",
+            data: { colour: "b" },
+            children: []
+          }]
+      }, {
+          id: "aviation",
+          name: "AVIATION",
+          data: {},
+          children: [{
+            id: "a1",
+            name: "Army",
+            data: { colour: "c" },
+            children: []
+          }, {
+            id: "a2",
+            name: "Air Cadets",
+            data: { colour: "b" },
+            children: [{
+              id: "a2.1",
+              name: "Flying Scholarship",
+              data: { colour: "a" },
+              children: [{
+                id: "a2.1.1",
+                name: "Commercial Pilot License",
+                data: { colour: "a" },
+                children: []
+              }, {
+                id: "a2.1.2",
+                name: "Apprentice Airline",
+                data: { colour: "c" },
+                children: []
+              }]
+            }]
+          }, {
+            id: "a3",
+            name: "Pilot License",
+            data: { colour: "a" },
+            children: []
+          }, {
+            id: "a4",
+            name: "RAF",
+            data: { colour: "c" },
+            children: [{
+              id: "a5.1",
+              name: "Engineer",
+              data: { colour: "c" },
+              children: []
+            }]
+          }, {
+            id: "a5",
+            name: "Engineer",
+            data: { colour: "c" },
+            children: []
+          }]
+      }]
+  };
